fc6db01056eb91751b212eb17206d4fd7a2b3ada
ntshovelom <ntshovelom@ZENSAR-619NS.zensar.local>
2018-08-22 10:58:10 +0200

    Adding Page Title

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Login</title>
     </head>
     <body>
         <?php
-        
+            
         ?>
     </body>
 </html>
